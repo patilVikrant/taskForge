c2abc67c13e4c8b851f700333d929e4a537ba271
Vikrant Patil <pvikrant248@gmail.com>
2026-06-17 00:41:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">TaskForge</h1>
+      <button className="btn btn-primary mt-3">Test Button</button>
+    </div>
+  );
+}
+
+export default App;
